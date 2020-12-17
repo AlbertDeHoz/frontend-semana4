6893ece9d74d331c23364c7ea562dab6ded9eb4c
Albert <alberto.entreprises@gmail.com>
2020-12-17 10:58:48 -0500

    Base para desarrollo del frontEnd del proyecto final

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import es from 'vuetify/lib/locale/es';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+      options: {
+        customProperties: true,
+      },
+    themes: {
+
+    },
+  },
+    lang: {
+      locales: { es },
+      current: 'es',
+    },
+});
