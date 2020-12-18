35e615de42d2b212a0541bc82c218bd1546b88e7
Albert <alberto.entreprises@gmail.com>
2020-12-17 20:27:29 -0500

    Se agregan las categoiras y vuetify

---
+++ src/views/categoria.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <data-categories/> 
+    </v-container>
+</template>
+
+<script>
+import DataCategories from '../components/DataCategories'
+export default {
+    name : 'categories',
+    components: {
+        DataCategories
+    }
+}
+</script>
